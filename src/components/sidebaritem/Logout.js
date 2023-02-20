de4352d9ce5c5bfd80bc0717bdaea36df1e6158d
BetterLogicTeam <102576102+BetterLogicTeam@users.noreply.github.com>
2023-02-21 01:19:04 +0500

    add LogOut

--- src/components/sidebaritem/Logout.js
+++ src/components/sidebaritem/Logout.js
@@ -3,9 +3,14 @@ import Passwordicon from "../../assets/images/Passwordicon.png";
 // import sidebarbootom from "../../assets/images/sidebarmainimage";
 
 import { Image } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 const Logout = () => {
+  const history = useNavigate()
   const LogoutHandler = (e) => {
-    e.preventDefault();
+    localStorage.setItem("isAuthenticated", false);
+    localStorage.setItem("user", null);
+    history("/Login_main");
+    window.location.reload();
   };
   return (
     <>
